e76aed45fdd2224a2b81e12c71b491d7f12689e3
ostapovych1983 <ostapovych1983@gmail.com>
2023-07-11 16:16:09 +0300

    Remove unnecessary exception constructors

--- src/main/java/ua/com/vasyl/ostapovych/dbf/dbaseframework/api/dbf/exceptions/DBFIllegalFieldException.java
+++ src/main/java/ua/com/vasyl/ostapovych/dbf/dbaseframework/api/dbf/exceptions/DBFIllegalFieldException.java
@@ -1,18 +1,9 @@
 package ua.com.vasyl.ostapovych.dbf.dbaseframework.api.dbf.exceptions;
 
 public class DBFIllegalFieldException extends RuntimeException {
-    public DBFIllegalFieldException() {
-    }
 
     public DBFIllegalFieldException(String message) {
         super(message);
     }
 
-    public DBFIllegalFieldException(String message, Throwable cause) {
-        super(message, cause);
-    }
-
-    public DBFIllegalFieldException(Throwable cause) {
-        super(cause);
-    }
 }
--- src/main/java/ua/com/vasyl/ostapovych/dbf/dbaseframework/api/dbf/exceptions/DBFNoSuchRowException.java
+++ src/main/java/ua/com/vasyl/ostapovych/dbf/dbaseframework/api/dbf/exceptions/DBFNoSuchRowException.java
@@ -3,8 +3,6 @@ package ua.com.vasyl.ostapovych.dbf.dbaseframework.api.dbf.exceptions;
 import java.util.NoSuchElementException;
 
 public class DBFNoSuchRowException extends NoSuchElementException {
-    public DBFNoSuchRowException() {
-    }
 
     public DBFNoSuchRowException(String s) {
         super(s);
